6b0bdbed46b0839afb3617b244e5b0f0dd09d6b4
Christian Acuña <cacuna0828@gmail.com>
2017-01-28 13:53:27 -0800

    add start meeting command

--- examples/components/teleporter.js
+++ examples/components/teleporter.js
@@ -1,8 +1,12 @@
+var accountSid = 'AC0639d6030e48d80dc09e9eddf6016622';
+var authToken = 'e38657d5b3e2c0c29afdceb12fe75a73';
+
+//require the Twilio module and create a REST client
+var client = require('twilio')(accountSid, authToken);
+
 AFRAME.registerSystem('teleporter', {
     init: function () {
         console.log("in teleporter system init");
-        this.teleportPosition = "0 0 0";
-        window.addEventListener('loaded', this.onSceneLoaded.bind(this));
     },
     onSceneLoaded: function(evt) {
         console.log("in voice-command system onSceneLoaded listener");
@@ -11,64 +15,49 @@ AFRAME.registerSystem('teleporter', {
 AFRAME.registerComponent('teleporter', {
 
     schema: {
-        character: { type: 'selector' },
-        marker: { type: 'selector' }
     },
     init: function () {
         console.log("in teleporter init");
-        this.isRaycasterActive = false;
-        this.el.teleportPoint = "0 0 0";
-        this.el.hasTeleportPoint = false;
-        this.el.addEventListener('raycaster-intersection', function (evt) {
-           // console.log('Raycast hit something!, evt intersection point: '+AFRAME.utils.coordinates.stringify(evt.detail.intersections[0].point));
-            //var teleportSystem = document.querySelector('a-scene').systems['teleporter'];
-            //teleportSystem.teleportPosition = "0 0 0";
-            var intersectionPoint = evt.detail.intersections[0].point;
-            this.teleportPoint = AFRAME.utils.coordinates.stringify(evt.detail.intersections[0].point);
-            this.hasTeleportPoint = true;
-            var markerPoint = intersectionPoint;
-            markerPoint.y = markerPoint.y + 0.8; // TODO: find better way to set character height
-            //console.log("in raycaster-intersection, components: "+this.components);
-            //console.log("in raycaster-intersection, teleporter: "+this.components['teleporter']);
-            var teleporterComponent = this.components['teleporter'];
-            if (teleporterComponent.isRaycasterActive) {
-                var marker = teleporterComponent.data.marker;
-                marker.setAttribute("position", AFRAME.utils.coordinates.stringify(markerPoint));
-                marker.setAttribute("visible", "true");
-            }
-        });
-        this.el.addEventListener('raycaster-intersection-cleared', function (evt) {
-           // console.log('Raycast intersection cleared!');
-            this.hasTeleportPoint = false;
-            var teleporterComponent = this.components['teleporter'];
-            var marker = teleporterComponent.data.marker;
-            marker.setAttribute("visible", "false");
-        });
     },
     showRaycaster: function() {
-        console.log("in showRaycaster");
-        this.isRaycasterActive = true;
-        this.el.setAttribute("material", "color: magenta; shader: flat");
+        console.log("working!!!!!!!!!!");
+        rest = "https://app.liveh2h.com/tutormeetweb/rest/v1/meetings/instant"
+        name = "Christian Acuna"
+        email = "cacuna0828@gmail.com"
+        var obj = {"name":name, "email":email};
+        var objstr = JSON.stringify(obj);
+        var meetingurl = "";
+        jQuery.ajax( {
+            url: rest,
+            type: 'POST',
+            data: objstr,
+            beforeSend : function( xhr ) {
+                xhr.setRequestHeader("Content-Type", "application/json");
+            },
+            success: function( response ) {
+                 meetingurl = response.data.meetingURL;
+                 console.log(response.data);
+                //  window.location = response.data.meetingURL
+                 window.open(response.data.meetingURL,"_blank");
+                 client.messages.create({
+                     to: "+1706-256-8014",
+                     from: "+15017250604",
+                     body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+                 }, function(err, message) {
+                     console.log(message.sid);
+                 });
+            },
+            error: function( response ) {
+                 if(document.getElementById( 'widgetFailureMessageEng' )){
+                 		document.getElementById( 'widgetFailureMessageEng' ).innerHTML ="You are missing a required field";
+                 }
+            }
+        } );
+
+        return false;
+
     },
-    teleport: function() {
-        console.log("in teleport, rayCasterActive: "+this.isRaycasterActive+", hasTeleportPoint: "+this.el.hasTeleportPoint);
-        if (this.isRaycasterActive && this.el.hasTeleportPoint) {
-            var teleportPoint = AFRAME.utils.coordinates.parse(this.el.teleportPoint);
-            teleportPoint.y = teleportPoint.y + 2; // TODO: find better way to set character height
-            character.setAttribute("position", AFRAME.utils.coordinates.stringify(teleportPoint));
-            this.data.marker.setAttribute("visible", "false");
-        }
-        this.isRaycasterActive = false;
-        this.el.setAttribute("material", "color: cyan; shader: flat");
-    },
-    cancel: function() {
-        console.log("in showRaycaster");
-        this.isRaycasterActive = false;
-        this.el.setAttribute("material", "color: cyan; shader: flat");
-        this.data.marker.setAttribute("visible", "false");
-    },
-    play: function() {
-        console.log("in teleporter play");
+    invite: function() {
+      console.log('invite!!!!!!!!!!!');
     }
 });
-
